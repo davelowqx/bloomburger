aa0c0ff3094a27526059f058cc416f25e4842f81
davelowqx <lowqixiang@gmail.com>
2021-06-30 01:24:42 +0800

    added api

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,76 @@ import Image from "next/image";
 import React from "react";
 import styles from "../styles/Home.module.css";
 
-export async function getServerSideProps() {
-  const data = await fetch(
-    `http://query2.finance.yahoo.com/v10/finance/quoteSummary/FB?modules=summaryDetail,incomeStatementHistory`
-  )
-    .then((res) => {
-      console.log(res);
-      return res.json();
-    })
-    .catch((error) => console.log(error));
-  return { props: { data } };
-}
+export default function Home() {
+  const [data, setData] = React.useState([]);
+  const [input, setInput] = React.useState("");
+  const [symbols, setSymbols] = React.useState([]);
+  React.useEffect(async () => {
+    const raw = await Promise.all(
+      symbols.map((symbol) =>
+        fetch(`http://localhost:3000/api/${symbol}`, {
+          headers: {
+            modules: [
+              "summaryDetail",
+              "incomeStatementHistory",
+              "incomeStatementHistoryQuarterly",
+              "cashFlowStatementHistory",
+              "cashFlowStatementHistoryQuarterly",
+              "balanceSheetStatementHistory",
+              "cashFlowStatementHistoryQuarterly",
+            ],
+          },
+        })
+          .then((res) => res.json())
+          .then((json) => {
+            const { result, error } = json.data.quoteSummary;
+            if (error === null) {
+              const {
+                summaryDetail,
+                incomeStatementHistoryQuarterly,
+                incomeStatementHistory,
+              } = result[0];
+              return {
+                summaryDetail,
+                incomeStatementHistoryQuarterly:
+                  incomeStatementHistoryQuarterly.incomeStatementHistory[0],
+                incomeStatementHistory:
+                  incomeStatementHistory.incomeStatementHistory[0],
+              };
+            }
+          })
+      )
+    );
+    setData(raw);
+  }, [symbols]);
 
-export default function Home({ data }) {
-  const [{ summaryDetail, incomeStatementHistory }] = data.quoteSummary.result;
+  const handleInput = (event) => setInput(event.target.value);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSymbols([...symbols, input]);
+    setInput("");
+  };
+
+  //const [{ summaryDetail, incomeStatementHistory }] = data.quoteSummary.result;
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={handleInput} />
+        <button>submit</button>
+      </form>
+
+      {data.map((company, i) => (
+        <div key={i}>{JSON.stringify(company)}</div>
+      ))}
+    </div>
+  );
+}
+
+/*
       <ul>
         <li>{summaryDetail.marketCap.raw}</li>
         <li>
           {incomeStatementHistory.incomeStatementHistory[0].totalRevenue.raw}
         </li>
       </ul>
-    </div>
-  );
-}
+      */
