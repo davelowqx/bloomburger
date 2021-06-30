f3f81f503cbf41566742e702113c98cd678925ca
davelowqx <lowqixiang@gmail.com>
2021-06-30 17:47:05 +0800

    react-table integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        crossorigin="anonymous"
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,230 +1,292 @@
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
+import { Button, Alert, Form } from "react-bootstrap";
+import BTable from "react-bootstrap/Table";
+import { useTable, useSortBy } from "react-table";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [data, setData] = React.useState([]);
   const [input, setInput] = React.useState("");
-  const [symbols, setSymbols] = React.useState([]);
+  const [symbols, setSymbols] = React.useState([
+    "FB",
+    "JNJ",
+    "XOM",
+    "GS",
+    "HON",
+    "DUK",
+    "AMZN",
+  ]);
   const [loading, setLoading] = React.useState(false);
-
-  const incomeStatementMapper = (obj) => {
-    return {
-      endDate: obj.endDate.fmt,
-      totalRevenue: obj.totalRevenue.raw,
-      grossProfit: obj.grossProfit.raw,
-      researchDevelopment: obj.researchDevelopment.raw,
-      sellingGeneralAdministrative: obj.sellingGeneralAdministrative.raw,
-      totalOtherIncomeExpenseNet: obj.totalOtherIncomeExpenseNet.raw,
-      operatingIncome: obj.operatingIncome.raw,
-      ebit: obj.ebit.raw,
-      netIncome: obj.netIncome.raw,
-    };
-  };
-  const incomeStatementReducer = (accum, curr) => {
-    return {
-      totalRevenue: curr.totalRevenue + accum.totalRevenue,
-      grossProfit: curr.grossProfit + accum.grossProfit,
-      researchDevelopment: curr.researchDevelopment + accum.researchDevelopment,
-      sellingGeneralAdministrative:
-        curr.sellingGeneralAdministrative + accum.sellingGeneralAdministrative,
-      totalOtherIncomeExpenseNet:
-        curr.totalOtherIncomeExpenseNet + accum.totalOtherIncomeExpenseNet,
-      operatingIncome: curr.operatingIncome + accum.operatingIncome,
-      ebit: curr.ebit + accum.ebit,
-      netIncome: curr.netIncome + accum.netIncome,
-    };
-  };
-
-  const cashFlowStatementMapper = (obj) => {
-    return {
-      endDate: obj.endDate.fmt,
-      depreciation: obj.depreciation.raw,
-      totalCashFromOperatingActivities:
-        obj.totalCashFromOperatingActivities.raw,
-      capitalExpenditures: obj.capitalExpenditures.raw,
-      investments: obj.investments.raw,
-      totalCashflowsFromInvestingActivities:
-        obj.totalCashflowsFromInvestingActivities.raw,
-      netBorrowings: obj.netBorrowings.raw,
-      totalCashFromFinancingActivities:
-        obj.totalCashFromFinancingActivities.raw,
-    };
-  };
-
-  const cashFlowStatementReducer = (accum, curr) => {
-    return {
-      depreciation: accum.depreciation + curr.depreciation,
-      totalCashFromOperatingActivities:
-        accum.totalCashFromOperatingActivities +
-        curr.totalCashFromOperatingActivities,
-      capitalExpenditures: accum.capitalExpenditures + curr.capitalExpenditures,
-      investments: accum.investments + curr.investments,
-      totalCashflowsFromInvestingActivities:
-        accum.totalCashflowsFromInvestingActivities +
-        curr.totalCashflowsFromInvestingActivities,
-      netBorrowings: accum.netBorrowings + curr.netBorrowings,
-      totalCashFromFinancingActivities:
-        accum.totalCashFromFinancingActivities +
-        curr.totalCashFromFinancingActivities,
-    };
-  };
-  const balanceSheetMapper = (obj) => {
-    return {
-      endDate: obj.endDate.fmt,
-      cash: obj.cash.raw,
-    };
-  };
+  const [error, setError] = React.useState("");
 
   React.useEffect(async () => {
-    const raw = await Promise.all(
-      symbols.map((symbol) =>
-        fetch(`/api/${symbol}`, {
-          headers: {
-            modules: [
-              "price",
-              "summaryProfile",
-              "incomeStatementHistory",
-              "incomeStatementHistoryQuarterly",
-              "cashflowStatementHistory",
-              "cashflowStatementHistoryQuarterly",
-              "balanceSheetHistory",
-              "balanceSheetHistoryQuarterly",
-              "summaryProfile",
-            ],
-          },
-        })
-          .then((res) => res.json())
-          .then((json) => {
-            const { result, error } = json.data.quoteSummary;
-            if (error === null) {
-              const {
-                price,
-                summaryProfile,
-                incomeStatementHistoryQuarterly,
-                incomeStatementHistory,
-                cashflowStatementHistory,
-                cashflowStatementHistoryQuarterly,
-                //balanceSheetHistory,
-                //balanceSheetHistoryQuarterly,
-              } = result[0];
-
-              return {
-                symbol: symbol.toUpperCase(),
-                name: price.longName,
-                sector: summaryProfile.sector,
-                industry: summaryProfile.industry,
-                marketCap: price.marketCap.raw,
-                /*
-                  incomeStatementHistory:
-                    incomeStatementHistory.incomeStatementHistory.map(
-                      incomeStatementMapper
-                    ),
-                    */
-                incomeStatementHistoryTTM:
-                  incomeStatementHistoryQuarterly.incomeStatementHistory
-                    .map(incomeStatementMapper)
-                    .reduce(incomeStatementReducer),
-
-                /*
-                  cashflowStatementHistory:
-                    cashflowStatementHistory.cashflowStatements.map(
-                      cashFlowStatementMapper
-                    ),
-                  cashflowStatementHistoryTTM:
-                    cashflowStatementHistoryQuarterly.cashflowStatements
-                      .map(cashFlowStatementMapper)
-                      .reduce(cashFlowStatementReducer),
-                  balanceSheetHistory:
-                    balanceSheetHistory.balanceSheetStatements.map(
-                      balanceSheetMapper
-                    ),
-                  balanceSheetHistoryQuarterly:
-                    balanceSheetHistoryQuarterly.balanceSheetStatements.map(
-                      balanceSheetMapper
-                    ),
-                      */
-              };
-            }
-          })
-      )
-    );
-    setData(raw);
-    setLoading(false);
-  }, [symbols]);
+    setLoading(true);
+    try {
+      const data = await Promise.all(
+        symbols.map((symbol) => fetchData(symbol))
+      );
+      setData(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
   const handleInput = (event) => setInput(event.target.value);
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    setError("");
     setLoading(true);
-    setSymbols([...symbols, input]);
     setInput("");
+    setSymbols([...symbols, input]);
+    try {
+      const addl = await fetchData(input);
+      console.log(addl);
+      setData([...data, addl]);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const handleDelete = () => {
+    setData([...data.slice(0, i), ...data.slice(i + 1, data.length)]);
+  };
+
+  const [headers, setHeaders] = React.useState([
+    "symbol",
+    "name",
+    "sector",
+    "industry",
+    "marketCap",
+    "totalRevenueTTM",
+    "grossProfitTTM",
+    "operatingIncomeTTM",
+    "ebitTTM",
+    "netIncomeTTM",
+    "totalCashFromFinancingActivitiesTTM",
+    "totalCashFromOperatingActivitiesTTM",
+    "totalCashflowsFromInvestingActivitiesTTM",
+    "totalCurrentAssets",
+    "totalCurrentLiabilities",
+  ]);
+
+  const columns = headers.map((header) => {
+    return { Header: header, accessor: header };
+  });
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input value={input} onChange={handleInput} />
-        <button disabled={loading}>submit</button>
-      </form>
-
-      <table>
-        <tr key={0}>
-          <th>symbol</th>
-          <th>name</th>
-          <th>sector</th>
-          <th>industry</th>
-          <th>marketCap</th>
-          <th>totalRevenueTTM</th>
-          <th>grossProfitTTM</th>
-          <th>researchDevelopmentTTM</th>
-          <th>sellingGeneralAdministrativeTTM</th>
-          <th>totalOtherIncomeExpenseNetTTM</th>
-          <th>operatingIncomeTTM</th>
-          <th>ebitTTM</th>
-          <th>netIncomeTTM</th>
-        </tr>
-        {data.map((company, i) => {
-          const {
-            symbol,
-            name,
-            sector,
-            industry,
-            marketCap,
-            incomeStatementHistoryTTM,
-          } = company;
-          const {} = incomeStatementHistoryTTM;
-
-          const {
-            totalRevenue,
-            grossProfit,
-            researchDevelopment,
-            sellingGeneralAdministrative,
-            totalOtherIncomeExpenseNet,
-            operatingIncome,
-            ebit,
-            netIncome,
-          } = incomeStatementHistoryTTM;
-          return (
-            <tr key={i + 1}>
-              <td>{symbol}</td>
-              <td>{name}</td>
-              <td>{sector}</td>
-              <td>{industry}</td>
-              <td>{marketCap}</td>
-              <td>{totalRevenue}</td>
-              <td>{grossProfit}</td>
-              <td>{researchDevelopment}</td>
-              <td>{sellingGeneralAdministrative}</td>
-              <td>{totalOtherIncomeExpenseNet}</td>
-              <td>{operatingIncome}</td>
-              <td>{ebit}</td>
-              <td>{netIncome}</td>
-            </tr>
-          );
-        })}
-      </table>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Control
+            value={input}
+            type="text"
+            placeholder="Symbol"
+            disabled={loading}
+            onChange={handleInput}
+          />
+        </Form.Group>
+        <Button disabled={loading}>submit</Button>
+        <Alert variant="danger" show={!!error}>
+          {error}
+        </Alert>
+      </Form>
+      <Table columns={columns} data={data} />
     </div>
+  );
+}
+
+const fetchData = async (symbol) => {
+  return fetch(`/api/${symbol}`, {
+    headers: {
+      modules: [
+        "price",
+        "summaryProfile",
+        "incomeStatementHistory",
+        "incomeStatementHistoryQuarterly",
+        "cashflowStatementHistory",
+        "cashflowStatementHistoryQuarterly",
+        "balanceSheetHistoryQuarterly",
+      ],
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error(res);
+      }
+    })
+    .then((json) => {
+      const { result, error } = json.data.quoteSummary;
+      if (error === null) {
+        const {
+          price,
+          summaryProfile,
+          incomeStatementHistoryQuarterly,
+          incomeStatementHistory,
+          cashflowStatementHistory,
+          cashflowStatementHistoryQuarterly,
+          balanceSheetHistoryQuarterly,
+        } = result[0];
+
+        return {
+          symbol: symbol.toUpperCase(),
+          name: price.longName,
+          sector: summaryProfile.sector,
+          industry: summaryProfile.industry,
+          marketCap: price.marketCap.raw,
+          ...incomeStatementHistory.incomeStatementHistory
+            .map(incomeStatementMapper)
+            .map((obj, i) => flatten(obj, i))
+            .reduce(arrToObj),
+          ...flatten(
+            incomeStatementHistoryQuarterly.incomeStatementHistory
+              .map(incomeStatementMapper)
+              .reduce(sum),
+            "TTM"
+          ),
+          ...cashflowStatementHistory.cashflowStatements
+            .map(cashFlowStatementMapper)
+            .map((obj, i) => flatten(obj, i))
+            .reduce(arrToObj),
+          ...flatten(
+            cashflowStatementHistoryQuarterly.cashflowStatements
+              .map(cashFlowStatementMapper)
+              .reduce(sum),
+            "TTM"
+          ),
+          ...balanceSheetHistoryQuarterly.balanceSheetStatements.map(
+            balanceSheetMapper
+          )[0],
+        };
+      } else {
+        throw new Error(error.code);
+      }
+    });
+};
+
+const sum = (accum, curr) => {
+  for (let key in accum) {
+    if (key !== "endDate") {
+      accum[key] += curr[key];
+    }
+  }
+  return accum;
+};
+
+const arrToObj = (accum, curr) => {
+  for (let key in curr) {
+    accum[key] = curr[key];
+  }
+  return accum;
+};
+
+const flatten = (obj, suffix) => {
+  const flattened = {};
+  for (let key in obj) {
+    flattened[`${key}${suffix}`] = obj[key];
+  }
+  return flattened;
+};
+
+const incomeStatementMapper = (obj) => {
+  const fields = [
+    "endDate",
+    "totalRevenue",
+    "grossProfit",
+    "operatingIncome",
+    "ebit",
+    "netIncome",
+  ];
+  const ret = { endDate: obj.endDate.fmt };
+  fields.forEach((field) => (ret[field] = obj[field].raw));
+  return ret;
+};
+
+const cashFlowStatementMapper = (obj) => {
+  const fields = [
+    "endDate",
+    "depreciation",
+    "totalCashFromOperatingActivities",
+    "capitalExpenditures",
+    "investments",
+    "totalCashflowsFromInvestingActivities",
+    "netBorrowings",
+    "totalCashFromFinancingActivities",
+  ];
+  const ret = { endDate: obj.endDate.fmt };
+  fields.forEach((field) => (ret[field] = obj[field].raw));
+  return ret;
+};
+
+const balanceSheetMapper = (obj) => {
+  const fields = [
+    "cash",
+    "totalCurrentAssets",
+    "totalAssets",
+    "totalCurrentLiabilities",
+    "totalLiab",
+    "totalStockholderEquity",
+  ];
+  const ret = {};
+  fields.forEach((field) => (ret[field] = obj[field].raw));
+  return ret;
+};
+
+function Table({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy
+    );
+  return (
+    <>
+      <BTable striped bordered hover size="sm" {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </BTable>
+      <br />
+    </>
   );
 }
