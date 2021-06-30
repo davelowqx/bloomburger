1e4f76b9403b7c8c1f14b35823a41e3be7f58926
davelowqx <lowqixiang@gmail.com>
2021-06-30 18:12:41 +0800

    crossorigin

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       />
       <Component {...pageProps} />;
     </>
